8d712ae08a080143085fe072a0cc76eff171c2a8
UggishResources <hugge202112@gmail.com>
2024-10-19 23:54:06 +0200

    Add files via upload

---
+++ u-vehiclespawner/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+author 'Uggishhh#4551'
+description 'Vehicle Spawner'
+
+shared_scripts {
+    'config.lua',
+    '@ox_lib/init.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+
